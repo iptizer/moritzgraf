afbb62f6195b2e80e64df5eb464a5dd7f991b2e2
Moritz Graf <moritz.graf@g-fit.de>
2019-10-19 13:41:20 +0200

    Putting container name into variable

--- README.md
+++ README.md
@@ -7,5 +7,10 @@ build.sh
 cat build.sh # to execute commands seperately
 ```
 
+Then ssh to the server and redeploy container:
 
-
+```bash
+ssh moritzgraf.de
+cd /home/docker/container/
+docker-compose-fullupgrade moritzgrafde
+```
--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
+CONTAINER_NAME="moritzgrafde"
 hugo
-docker build -t moritzgrafde:latest -t hub.moritzgraf.de:5000/moritzgrafde:latest .
-#docker tag moritzgrafde:latest iptize.it:5000/moritzgrafde:latest
-docker push hub.moritzgraf.de:5000/moritzgrafde:latest
+docker build -t ${CONTAINER_NAME}:latest -t hub.moritzgraf.de:5000/${CONTAINER_NAME}:latest .
+docker push hub.moritzgraf.de:5000/${CONTAINER_NAME}:latest
