8b382375a71d786598fd783ade9a6df790a2bc84
Moritz Graf <github@moritzgraf.de>
2021-08-29 20:21:25 +0200

    Adding build script so that it automatically redeploys

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 CONTAINER_NAME="moritzgrafde"
-#DEPLOYMENT="moritzgrafde"
-#NAMESPACE="web"
+DEPLOYMENT="moritzgrafde"
+NAMESPACE="web"
 
 hugo
 
 docker build -t ${CONTAINER_NAME}:latest -t registry.haumdaucher.de/${CONTAINER_NAME}:latest .
 docker push registry.haumdaucher.de/${CONTAINER_NAME}:latest
 
-#kubectl patch deployment $DEPLOYMENT -n $NAMESPACE -p "{\"spec\": {\"template\": {\"metadata\": { \"labels\": {  \"redeploy\": \"$( date +%s )\"}}}}}"
-#kubectl get po -l app=$DEPLOYMENT -n $NAMESPACE -w
+kubectl patch deployment $DEPLOYMENT -n $NAMESPACE -p "{\"spec\": {\"template\": {\"metadata\": { \"labels\": {  \"redeploy\": \"$( date +%s )\"}}}}}"
+kubectl get po -l app=$DEPLOYMENT -n $NAMESPACE -w
